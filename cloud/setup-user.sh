569e7fe3e51788bf282797f44ce639a39083e5b2
pardhu <60252032+pardhukatta@users.noreply.github.com>
2021-07-16 13:23:34 +0530

    Update setup-user.sh

--- cloud/setup-user.sh
+++ cloud/setup-user.sh
@@ -105,9 +105,9 @@ update_conf()
 
 ############### MAIN ###################
 
-USER="devops"
+USER="katta"
 GROUP="devops"
-passw="today@1234"
+passw="katta007"
 
 if [ -f /etc/os-release ];then
    osname=`grep ID /etc/os-release | egrep -v 'VERSION|LIKE|VARIANT' | cut -d'=' -f2 | sed -e 's/"//' -e 's/"//'`
